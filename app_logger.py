c16f0b3f989a52009063354b8f8ae7ad50784f24
ranvane <ranvane@gmail.com>
2025-03-25 22:40:13 +0800

    优化代码

--- app_logger.py
+++ app_logger.py
@@ -1,11 +1,14 @@
 import os
 import sys
 import logging
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s | %(levelname)-5s | %(filename)s | %(funcName)s | %(lineno)d | %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S'
-)
+
+def setup_logging(level, is_production):
+    logging.basicConfig(
+        level=level,
+        format='%(asctime)s | %(levelname)-5s | %(filename)s | %(funcName)s | %(lineno)d | %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+
 # 定义环境标志
 IS_PRODUCTION = os.path.dirname(os.path.abspath(
     sys.argv[0])) == '/usr/local/bin'
@@ -14,14 +17,7 @@ logging.info(f"IS_PRODUCTION: {IS_PRODUCTION}")
 if IS_PRODUCTION:
     # 生产环境：使用 /usr/share/wallpaper-changer
     RESOURCE_PATH = "/usr/share/wallpaper-changer"
-
-    logging.basicConfig(
-        level=logging.INFO,
-        format='%(asctime)s | %(levelname)-5s | %(filename)s | %(funcName)s | %(lineno)d | %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S'
-    )
-
-
+    setup_logging(logging.INFO, IS_PRODUCTION)
 else:
     # 开发环境：使用当前文件所在目录
     RESOURCE_PATH = os.path.dirname(os.path.abspath(__file__))

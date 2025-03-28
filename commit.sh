e5379a458e6e89dcc2edf80a1247cee79880be0f
ranvane <ranvane@gmail.com>
2025-03-28 20:45:02 +0800

    优化DEBIAN/control

--- commit.sh
+++ commit.sh
@@ -2,6 +2,7 @@ echo  "设置Git代理"
 # 设置http、https::
 git config --global http.proxy http://127.0.0.1:7897
 git config --global https.proxy https://127.0.0.1:7897
+
 # 设置socks:
 git config --global http.proxy 'socks5://127.0.0.1:7897'
 git config --global https.proxy 'socks5://127.0.0.1:7897'
